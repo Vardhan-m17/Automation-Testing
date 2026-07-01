00de6932a742e2ad2e40c107b6b01f5d1254f97c
Vardhan-m17 <vardhanmudra12@gmail.com>
2026-07-01 11:22:30 +0530

    Area of the rectangle

---
+++ Lab005.java
@@ -0,0 +1,17 @@
+package Lab005;
+
+public class Lab005 {
+	
+public static void main(String[] args) {
+		
+		int length =10;
+		int breadth =5;
+		
+		int area=length*breadth;
+		
+		System.out.println(area);
+		
+	}
+
+
+}
