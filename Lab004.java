e110358bedc4159b8700c769f6256ef7294a60dc
Vardhan-m17 <vardhanmudra12@gmail.com>
2026-07-01 11:21:55 +0530

    Swap of two numbers

---
+++ Lab004.java
@@ -0,0 +1,25 @@
+package Lab004;
+
+public class Lab004 {
+	
+public static void main(String[] args) {
+		
+		int a=5;
+		int b=10;
+		int temp;
+		
+		temp = a;
+		
+		a = b;
+		
+		b=temp;
+		
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+	}
+
+
+
+}
