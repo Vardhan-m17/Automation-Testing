f4ae29dc69c98fa8fcc70df7ac094544533ad81e
Vardhan-m17 <vardhanmudra12@gmail.com>
2026-07-01 11:42:15 +0530

    Display ASCII Value of a Character

---
+++ Lab007.java
@@ -0,0 +1,18 @@
+package Lab007;
+
+public class Lab007 {
+	
+	public static void main(String[] args) {
+		
+		char ch = 'A';
+		
+		int Ascii = ch;
+		
+		System.out.println("Character = " + ch);
+		
+		System.out.println("Integer =" + Ascii);
+		
+		
+	}
+
+}
