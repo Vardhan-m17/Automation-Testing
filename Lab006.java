045cedbe0e6821941c1e3af9bd9b2b96d5c92cda
Vardhan-m17 <vardhanmudra12@gmail.com>
2026-07-01 11:23:59 +0530

    Average of three numbers

---
+++ Lab006.java
@@ -0,0 +1,18 @@
+package Lab006;
+
+public class Lab006 {
+	
+public static void main(String[] args) {
+		
+		int a=10;
+		int b=15;
+		int c=20;
+		
+		int avg=(a+b+c)/3;
+		
+		System.out.println(avg);
+		
+		
+	}
+
+}
