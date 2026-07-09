50d764d72de7f0ad47b0ca07dcdd4df1718ed562
Vardhan-m17 <vardhanmudra12@gmail.com>
2026-07-09 13:25:25 +0530

    Login page Validation

---
+++ Lab008.java
@@ -0,0 +1,30 @@
+package Lab008;
+
+public class Lab008 {
+	
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		String username = "Admin";
+		String password = "12345";
+		
+		if(username.equals("Admin")  && password.equals("12345"))
+		
+		{
+			
+			System.out.println("Login Successful");
+			
+		}
+		
+		else 
+		
+		{
+			
+			System.out.println("Login Failed");
+		}
+		
+	}
+	
+
+}
