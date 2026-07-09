b426b4fd8ccf8472d58a84e7e7ef6eaff7131416
Vardhan-m17 <vardhanmudra12@gmail.com>
2026-07-09 13:15:53 +0530

    Eligible for vote

---
+++ Lab009.java
@@ -0,0 +1,26 @@
+package Lab009;
+
+public class Lab009 {
+	
+	public static void main(String[] args) {
+		
+		int age = 19;
+		
+		
+		if(age>= 18)
+			
+		{
+			
+			System.out.println("Eligible for vote");
+		}
+		
+		else
+			
+		{
+			System.out.println("Not eligible for vote");
+			
+		}
+		
+	}
+
+}
